10cb7cec4c54c0c2e2571e599501fab41912750c
Andrii Kmit <andrikmit241@gmail.com>
2022-04-09 14:52:33 +0300

    added header and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BanksComponent } from "./pages/banks/banks.component";
+import { CalculatorComponent } from "./pages/calculator/calculator.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BanksComponent},
+  {path: 'mortgage-calculator', component: CalculatorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from "./shared/shared/shared.module";
+import { BanksComponent } from './pages/banks/banks.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BanksComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
